1b492bded287a32e71a47dfd120ebe2333ff5bab
kdyson <kdyson@users.noreply.github.com>
2020-12-04 13:25:09 -0800

    adding files
    setting up the repository

---
+++ WHERE_processing.R
@@ -0,0 +1,13 @@
+### ------ DATA PROCESSING ---------- ###
+
+## This file imports the raw data .csv and converts it into an analysis-ready
+## table for creating percentages and figures for the WHERE paper. The data
+## comes from the Data Collection WHERE only Excel workbook. Data was collected
+## by two authors, Karen Dyson & Emad Dawwas in 2020.
+
+## The full workflow for this project is as follows: data collection (Data
+## Collection WHERE only Excel) --> compile all journal information and export
+## as .csv ()
+
+
+read.csv("..")
