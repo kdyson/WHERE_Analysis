cb7594082c9a132744b3cadd990c9d2dc9b17483
kdyson <kdyson@users.noreply.github.com>
2020-12-04 15:05:16 -0800

    processing
    initial processing pass

---
+++ WHERE_analysis.R
@@ -0,0 +1,14 @@
+### ------ DATA ANALYSIS ---------- ###
+
+## This file analyzes the analysis-ready data created in the sister file
+## WHERE_processing.R. Here we create the percentages and figures needed for
+## publication.
+
+## The full workflow for this project is as follows: data collection (Data
+## Collection WHERE only Excel) --> compile all journal information and export
+## as .csv (Combined Data WHERE.csv) --> process raw data into analysis-ready
+## table (WHERE_processing.R) --> analyze data and create figures for publication
+## (this file, WHERE_analysis.R)
+
+source("WHERE_processing.R")
+
--- WHERE_processing.R
+++ WHERE_processing.R
@@ -7,7 +7,58 @@
 
 ## The full workflow for this project is as follows: data collection (Data
 ## Collection WHERE only Excel) --> compile all journal information and export
-## as .csv ()
+## as .csv (Combined Data WHERE.csv) --> process raw data into analysis-ready
+## table (this file, WHERE_processing.R) --> analyze data and create figures for
+## publication (WHERE_analysis.R)
+
+library(reshape2)
 
 
-read.csv("..")
+### -------- INGEST DATA ---------------------- ###
+
+WHERE_data <- read.csv("Combined Data WHERE.csv", blank.lines.skip = TRUE, stringsAsFactors = F)
+
+### ------- MANIPULATE DATA -------------------- ###
+
+## We need to create proper column and row names, and then transpose the data
+
+#First, remove all of the blank rows.
+WHERE_data <- WHERE_data[ !is.na(WHERE_data$ï..QID), ]
+
+# Now create column names that make sense (these will become rownames)
+colnames(WHERE_data)[1] <- "QID"
+colnames(WHERE_data)[2] <- "Q_text"
+
+# create text to concatinate into a readible paper id
+paper.IDs <- as.character(unlist(WHERE_data[WHERE_data$QID == 7, 3:ncol(WHERE_data)], use.names = F))
+reviewer <- as.character(unlist(WHERE_data[WHERE_data$QID == 8, 3:ncol(WHERE_data)], use.names = F))
+reviewer <- ifelse(reviewer == "EM-KD", "EMAD",
+                   ifelse(reviewer == "KD-EM", "EMAD",
+                          ifelse(reviewer == "KD & TF", "KD",
+                                 ifelse(reviewer == "", "KD", reviewer))))
+
+paper.IDs <- paste0(paper.IDs, "-", reviewer)
+
+# this cleans up the reviewer column also for future selection
+  WHERE_data[WHERE_data$QID == 8, 3:ncol(WHERE_data)] <- reviewer 
+
+
+colnames(WHERE_data)[3:ncol(WHERE_data)] <- paper.IDs
+  
+
+# Now create rownames that make sense (these will become colnames)
+
+WHERE_data$QID <- paste0("Q", WHERE_data$QID)
+
+rownames(WHERE_data) <- WHERE_data$QID
+
+# transpose the data
+WHERE_data <- as.data.frame(t(WHERE_data), stringsAsFactors = F)
+
+
+
+### ---------- SUBSET THE DATA ----------- ###
+
+WHERE_data_KD <- WHERE_data[WHERE_data$Q8 == "KD",]
+
+
